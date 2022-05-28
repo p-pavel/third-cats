50100a92a3c4d16bc8a5d5ae0aeb88e111a439a8
Pavel Perikov <perikov@gmail.com>
2022-05-28 16:47:32 +0400

    Arr,Obj,Category,Functor,Natural

---
+++ build.sbt
@@ -0,0 +1,9 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.1.2"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "third-cats",
+    scalacOptions ++= Seq("-source:future-migration", "-Xfatal-warnings" )
+  )
--- src/main/scala/com/perikov/thirdcats.scala
+++ src/main/scala/com/perikov/thirdcats.scala
@@ -1,107 +1,72 @@
 package com.perikov
 
-type Arr = {
+trait Arr:
   type Dom
   type Codom
-}
-
 object Arr:
-  type Aux[Obj <: Arr,A,B] = Obj {type Dom = A; type Codom = B}
-  def mark[T[_,_], A, B](a: T[A,B]): T[A,B] & Arr  = a.asInstanceOf
+  type Aux[T<: Arr, A,B] = T{type Dom = A; type Codom = B}
 
-trait ArrTrait extends Any{
-  type Dom
-  type Codom
-}
-
+type Obj = { type Repr}
+object Obj:
+  type Aux[O <: Obj,R] = O {type Repr = R}
 
 trait Category:
-  type IsObject[_]
-  type Arrow <: Arr
-  type HomSet[A,B] = Arrow {type Dom = A; type Codom = B}
-  def compose(f: Arrow, g: HomSet[?, f.Dom] ): HomSet[g.Dom,f.Codom]
-  def id[A](using IsObject[A]): HomSet[A,A]
-  def arrowIndexedByObjects(arr: Arrow): (IsObject[arr.Dom], IsObject[arr.Codom])
+  type O <: Obj
+  type A <: Arr
+  def compose(f: A, g: Arr.Aux[A,? ,f.Dom]): Arr.Aux[A,g.Dom,f.Codom]
+  def id[T](using Obj.Aux[O,T]): Arr.Aux[A,T,T]
 
 object Category:
-  type Aux[isObj[_], arr <: Arr] = Category {type IsObject[t]=isObj[t]; type Arrow = arr}
+  type Aux[o <: Obj,a <: Arr] = Category { type O = o; type A=a}
 
+trait Intercategory:
+  val srcCat: Category
+  val dstCat: Category
 
-object ScalaCat extends Category:
-  opaque type IsObject[T] = Unit
-  inline given [T]: IsObject[T] = ()
-  trait Arrow extends ArrTrait:
-    val function: Dom => Codom
-  class ArrImpl[A,B](val function: A => B) extends Function[A,B],Arrow:
-    type Dom = A; type Codom = B
-    def apply(a:A): B = function(a)
-  inline given [A,B]: Conversion[A => B, ArrImpl[A,B]] = ArrImpl(_)
-  def compose(f: Arrow, g: HomSet[?, f.Dom] ): HomSet[g.Dom,f.Codom] = ArrImpl(g.function andThen f.function)
-  def id[A](using IsObject[A]): HomSet[A,A] = ArrImpl(identity)
-  def arrowIndexedByObjects(arr: Arrow): (IsObject[arr.Dom], IsObject[arr.Codom]) = (summon,summon)
-
-trait TypeFunc:
+trait FunctorBase extends Intercategory, Arr:
+  type Dom   = srcCat.type
+  type Codom = dstCat.type
   type F[_]
 
-trait Functor extends ArrTrait, TypeFunc:
-  type Dom <: Arr
-  type Codom <: Arr
-  def fmap(arg: Dom): Arr.Aux[Codom, F[arg.Dom],F[arg.Codom]]
-object Functor {
-  type Aux[G[_],A,B] = Arr.Aux[Functor,A,B] {type F[t] = G[t]}
-}
 
-type Id[t] = t
-object CategoryOfCategories extends Category:
-  type IdFunctor[A ] = Functor.Aux[Id, A, A]
-  sealed trait IsObject[A]:
-    def id: IdFunctor[A]
+sealed trait Functor extends FunctorBase:
+  def fmap(arg: srcCat.A): Arr.Aux[dstCat.A, F[arg.Dom],F[arg.Codom]]
 
-  given [T <: Arr]: IsObject[T] with
-    def id = new Functor {
-      type Dom = T; type Codom = T; type F[t] = t
-      def fmap(arg: Dom): Arr.Aux[Codom, F[arg.Dom],F[arg.Codom]] = arg
-    }
+sealed trait ContraFunctor extends FunctorBase:
+  def fmap(arg: srcCat.A): Arr.Aux[dstCat.A, F[arg.Codom],F[arg.Dom]]
 
-  type Arrow = Functor
-  def compose(f: Arrow, g: HomSet[?, f.Dom] ): HomSet[g.Dom,f.Codom] = new Functor {
-    type Dom = g.Dom
-    type Codom = f.Codom
-    type F[t] = f.F[g.F[t]]
-    def fmap(arg: Dom): Arr.Aux[Codom, F[arg.Dom],F[arg.Codom]] = f.fmap(g.fmap(arg))
-  }
-  def id[A](using o: IsObject[A]): IdFunctor[A] = o.id
-  def arrowIndexedByObjects(arr: Arrow): (IsObject[arr.Dom], IsObject[arr.Codom]) = (summon, summon)
 
-trait IsFunctor[F[_],]
+object Functor:
+  type Aux[G[_], c1 <: Arr, c2 <: Arr] = Functor {type F[t] = G[t]; type Dom = c1; type Codom = c2}
+  def apply[G[_]](c1: Category, c2: Category, func: (arg: c1.A) => Arr.Aux[c2.A,G[arg.Dom], G[arg.Codom]] ):
+    Functor.Aux[G,c1.A, c2.A ] =
+    new Functor:
+      val srcCat:c1.type = c1
+      val dstCat:c2.type = c2
+      type F[t] = G[t]
+      def fmap(arg: srcCat.A): Arr.Aux[dstCat.A, F[arg.Dom],F[arg.Codom]] = func(arg)
 
-//class NatHom (val srcCat: Category, val dstCat: Category) extends Category:
-//  type FuncAux[G[_]] = Functor.Aux[G, srcCat.Arrow, dstCat.Arrow]
-//  trait Arrow extends ArrTrait:
-//    type F1[_]
-//    type F2[_]
-//    type Dom <: FuncAux[F1]
-//    type Codom <: FuncAux[F2]
-//    def apply[A](using srcCat.IsObject[A]): dstCat.HomSet[F1[A],F2[A]]
-//
-//  type Aux[f1[_],g1[_], func1 <: FuncAux[f1], func2: FuncAux[f2]
-//
-//  sealed trait IsObject[A]
-//  object IsObject:
-//    def apply[G[_], Func <: FuncAux[G]]=
-//      new IsObject[Func]:
-//        def id = new Arrow:
-//          type F1[t] = G[t]
-//          type F2[t] = G[t]
-//          type Dom = Func
-//          type Codom = Func
-//          def apply[A](using srcCat.IsObject[A]): dstCat.HomSet[F1[A],F2[A]] =
-//
-//  type Aux[f1[_],f2[_]] = Arrow {type F1[t] = f1[t]; type F2[t] = f2[t]}
-//  type IdNat[F[_]] = Aux[F,F]
-//  def compose(f: Arrow, g: HomSet[?, f.Dom] ): HomSet[g.Dom,f.Codom] = ???
-//  def id[A](using IsObject[A]): HomSet[A,A] = ???
-//  def arrowIndexedByObjects(arr: Arrow): (IsObject[arr.Dom], IsObject[arr.Codom]) = ???
-//
-//TODO: ScalaFunctor
-//TODO: ScalaNat
+
+trait Natural extends Intercategory:
+  val dom  : Functor
+  val codom: Functor
+  type Dom   =  dom.type
+  type Codom = codom.type
+  def apply[T](using o: Obj.Aux[srcCat.O,T]): Arr.Aux[dstCat.A, dom.F[T], codom.F[T]]
+
+object Natural:
+  def apply(
+            func1: Functor,
+            func2: Arr.Aux[Functor,func1.Dom, func1.Codom],
+            trans: [T] => (o: Obj.Aux[func1.srcCat.O, T]) ?=> Arr.Aux[func1.dstCat.A, func1.F[T], func2.F[T]]
+           ) =
+    new Natural:
+      val srcCat: func1.srcCat.type = func1.srcCat
+      val dstCat: func1.dstCat.type  = func1.dstCat
+      val dom: func1.type   = func1
+      val codom: func2.type = func2
+      def apply[T](using o: Obj.Aux[srcCat.O,T]): Arr.Aux[dstCat.A, dom.F[T], codom.F[T]] = trans(using o)
+
+
+
+
