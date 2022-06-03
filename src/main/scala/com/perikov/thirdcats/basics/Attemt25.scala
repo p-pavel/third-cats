6c7edebdce1aefef0c40315adc283d5b97277b4f
Pavel Perikov <perikov@gmail.com>
2022-06-03 14:14:58 +0400

    remove types from Nat

--- src/main/scala/com/perikov/thirdcats/basics/Attemt25.scala
+++ src/main/scala/com/perikov/thirdcats/basics/Attemt25.scala
@@ -49,4 +49,58 @@ object Attemt25 {
           t4
 
   inline given composableNat[c1 <: Arr, c2 <: Arr](using c:Composable[c2]): Composable[Nat[c1,c2]] = new composableNat[c1,c2]
+
+  trait FuncWrapper extends Arr{
+    val function: Dom => Codom
+    def apply(a: Dom): Codom = function(a)
+  }
+  object FuncWrapper:
+    inline def apply[A,B](f: A=>B):Arr.Aux[FuncWrapper,A,B] =
+      new FuncWrapper:
+        override val function = f
+        override type Dom = A
+        override type Codom = B
+
+  object ScalaCategory extends Composable[FuncWrapper]:
+    type A = FuncWrapper
+    def compose(a1: A , a2: A ) (using p:a2.Codom =:= a1.Dom): Arr.Aux[A, a2.Dom, a1.Codom] =
+      new FuncWrapper:
+        type Dom = a2.Dom
+        type Codom = a1.Codom
+        val function = (a: Dom) => a1(p(a2(a)))
+
+  given Composable[FuncWrapper] = ScalaCategory
+
+  given [A,B]: Conversion[A => B, Arr.Aux[FuncWrapper,A,B]] =
+    (func: A => B) => new FuncWrapper:
+      override val function = func
+      override type Dom = A
+      override type Codom = B
+
+  trait ScalaFunctor extends Functor:
+    type Dom = FuncWrapper
+    type Codom = FuncWrapper
+
+  def idFunctor[A <: Arr]: Functor {type F[t] = t; type Dom = A; type Codom = A} =
+    new Functor:
+      type F[t] = t
+      type Dom = A
+      type Codom = A
+      def apply(arg: A):Arr.Aux[A,arg.Dom, arg.Codom] = arg
+
+  def compose[A <: Arr](a1: A, a2: A)(using c: Composable[A], eq: a2.Codom =:= a1.Dom): Arr.Aux[A, a2.Dom, a1.Codom] =
+    c.compose(a1,a2)
+
+  object ListFunctor extends ScalaFunctor:
+    type F[t] = List[t]
+    def apply[A,B](f: A => B): List[A] => List[B] = _.map(f)
+
+  object OptionFunctor extends ScalaFunctor:
+    type F[t] = Some[t]
+    def apply[A,B](f: A => B): Option[A] => Option[B] = _.map(f)
+
+  val ScalaIdFunctor = idFunctor[FuncWrapper]
+  type ScalaNat = Nat[FuncWrapper, FuncWrapper]
+
+//  object id2List extends Nat[FuncWrapper,FuncWrapper]
 }
