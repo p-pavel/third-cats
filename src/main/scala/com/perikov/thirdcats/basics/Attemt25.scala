a154fb0c56a5300a5721ad51a91c38ad7f82ead0
Pavel Perikov <perikov@gmail.com>
2022-06-03 13:00:00 +0400

    Compiles

--- src/main/scala/com/perikov/thirdcats/basics/Attemt25.scala
+++ src/main/scala/com/perikov/thirdcats/basics/Attemt25.scala
@@ -1,44 +1,52 @@
 package com.perikov.thirdcats.basics
 
+import com.perikov.thirdcats.basics.Attemt25.Arr.Aux
+
 object Attemt25 {
   trait Arr:
     type Dom
     type Codom
 
-//  trait Functor extends Arr :
-//    type F[_]
-//
-//    def apply[T1, T2](arg: Dom {type Dom = T1; type Codom = T2}):
-//    Codom {type Dom = F[T1]; type Codom = F[T2]}
+  object Arr:
+    type Aux[A <: Arr, t1, t2] = A {type Dom = t1; type Codom = t2}
 
-  trait Composable[A <: Arr]:
-    def compose(a1: A, a2: A {type Codom = a1.Dom}): A {type Dom = a2.Dom; type Codom = a1.Codom}
+  trait Functor extends Arr :
+    type F[_]
+    type Dom <: Arr
+    type Codom <: Arr
 
-//    object FunctorComposable extends Composable[Functor]:
-//      type A = Functor
-//      def compose(
-//        a1: A, a2: A {type Codom = a1.Dom}): A {type Dom = a2.Dom; type Codom = a1.Codom} =
-//        new Functor:
-//          type F[t] = a1.F[a2.F[t]]
-//          type Dom = a2.Dom
-//          type Codom = a1.Codom
-//          def apply[T1,T2](arg: Dom {type Dom = T1; type Codom = T2}):
-//            Codom{type Dom = F[T1]; type Codom = F[T2]} =
-//              a1(a2(arg))
+  object Functor:
+    type Aux[G[_],A1 <: Arr, A2 <: Arr] = Functor {type F[t] = G[t]; type Dom = A1; type Codom = A2}
 
-    trait Nat[C1 <: Arr, C2 <: Arr] extends Arr
-//      type F1[_]
-//      type F2[_]
-//      type Dom = Functor {type Dom = C1; type Codom = C2; type F[t] = F1[t]}
-//      type Codom = Functor {type Dom = C1; type Codom = C2; type F[t] = F2[t]}
-//      def apply[T]: C2 {type Dom = F1[T]; type Codom = F2[T]}
+  trait Nat[C1 <: Arr, C2 <: Arr] extends Arr:
+    self=>
+    type F1[_]
+    type F2[_]
+    type Dom = Functor.Aux[F1,C1,C2]
+    type Codom = Functor.Aux[F2,C1,C2]
+    def apply[T]: Arr.Aux[C2, self.Dom#F[T], self.Codom#F[T] ]
 
-    class NatComposable[C1 <: Arr, C2 <: Arr](using comp: Composable[C2]) extends Composable[Nat[C1,C2]]:
-      def compose(a1: Nat[C1,C2], a2: Nat[C1,C2] {type Codom = a1.Dom}): Nat[C1,C2] {type Dom = a2.Dom; type Codom = a1.Codom} =
-        new Nat[C1,C2] {type Dom = a2.Dom; type Codom = a1.Codom}
-//          type F1[t] = a2.F1[t]
-//          type F2[t] = a1.F2[t]
-//          def apply[T]: C2 {type Dom = F1[T]; type Codom = F2[T]} = ???
+  trait Composable[ A <: Arr] :
+    def compose(a1: A , a2: A ) (using a2.Codom =:= a1.Dom): Arr.Aux[A, a2.Dom, a1.Codom]
 
 
+
+  class composableNat[c1 <: Arr, c2 <: Arr](using comp: Composable[c2]) extends Composable[ Nat[c1,c2]]:
+    type A = Nat[c1,c2]
+    def compose(a1: A , a2: A ) (using proof: a2.Codom =:= a1.Dom):
+    A {type Dom = a2.Dom; type Codom = a1.Codom} =
+      new Nat[c1,c2]:
+        self=>
+        type F1[t] = a2.F1[t]
+        type F2[t] = a1.F2[t]
+        def apply[T]: c2{type Dom = self.Dom#F[T]; type Codom = self.Codom#F[T] } =
+          val t2: Arr.Aux[c2,a2.Dom#F[T], a2.Codom#F[T]] = a2[T]
+          val t1: Arr.Aux[c2,a1.Dom#F[T], a1.Codom#F[T]] = a1[T]
+          val postulateToWorkAroundTypeSystemLimitation: a2.Codom#F[T] =:= a1.Dom#F[T] = proof.asInstanceOf
+          val tst = postulateToWorkAroundTypeSystemLimitation.liftCo[[q]=>>Arr.Aux[c2,a2.Dom#F[T], q]]
+          val t3: Aux[c2, a2.F1[T], a1.F1[T]] = tst(t2)
+          val t4  = comp.compose(t1,t3)
+          t4
+
+  inline given composableNat[c1 <: Arr, c2 <: Arr](using c:Composable[c2]): Composable[Nat[c1,c2]] = new composableNat[c1,c2]
 }
