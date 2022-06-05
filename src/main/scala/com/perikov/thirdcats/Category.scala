41521fb1f4808553a0d6979df03cfc324f68ceed
Pavel Perikov <perikov@gmail.com>
2022-06-05 12:02:10 +0400

    NatCategory

--- src/main/scala/com/perikov/thirdcats/Category.scala
+++ src/main/scala/com/perikov/thirdcats/Category.scala
@@ -1,9 +1,22 @@
 package com.perikov.thirdcats
 
-trait IsComposable[A <: Arrow]:
+import com.perikov.thirdcats.Arrow.Aux
+
+trait ComposableBase:
+  type A <: Arrow
   def compose(a1: A, a2: A)(using a2.Codom =:= a1.Dom): Arrow.Aux[A, a2.Dom, a1.Codom]
 
+trait IsComposable[B <: Arrow] extends ComposableBase:
+  type A = B
 
-trait Category[A <: Arrow] extends IsComposable[A]:
+trait CategoryBase extends ComposableBase:
   type IsObj[t]
   def id[t](using o: IsObj[t]): Arrow.Id[A,t]
+
+
+
+trait Category[B <: Arrow] extends ComposableBase:
+  type A = B
+
+
+
--- src/main/scala/com/perikov/thirdcats/Nat.scala
+++ src/main/scala/com/perikov/thirdcats/Nat.scala
@@ -1,42 +1,45 @@
 package com.perikov.thirdcats
 
-trait Nat[C1 <: Arrow, C2 <: Arrow] extends Arrow :
-  self =>
-  type F1[_]
-  type F2[_]
-  type Dom = Functor.Aux[F1, C1, C2]
-  type Codom = Functor.Aux[F2, C1, C2]
-  def apply[T]: Arrow.Aux[C2, self.Dom#F[T], self.Codom#F[T]]
-  
-given natCategory[c1 <: Arrow, c2 <: Arrow] (using c1Cat: Category[c1], c2Cat:Category[c2]): Category[Nat[c1,c2]]
-  with
 
-  type A = Nat[c1,c2]
-  trait IsObj[t]:
-    def idNat: Arrow.Id[A,t]
+class NatCategory(val c1: CategoryBase, val c2: CategoryBase) extends CategoryBase:
+  trait Nat extends Arrow:
+    type F1[_]
+    type F2[_]
+    type Dom = Functor.Aux[F1,c1.A, c2.A]
+    type Codom = Functor.Aux[F2, c1.A, c2.A]
+    def apply[T](using obj: c1.IsObj[T]): Arrow.Aux[c2.A, Dom#F[T], Codom#F[T]]
+
+  type A = Nat
+
+  trait IsObj[T]:
+    type F[_]
+    def idNat: Arrow.Id[Nat, T]
+
   object IsObj:
-    def apply[G[_]](func: Functor.Aux[G,c1,c2]): IsObj[Functor.Aux[G,c1,c2]]=
-      new IsObj[Functor.Aux[G,c1,c2]]:
-        def idNat: Arrow.Id[A, Functor.Aux[G,c1,c2]] =
-          new Nat[c1,c2]:
-            type F1[t] = G[t]
-            type F2[t] = G[t]
-            def apply[T]: Arrow.Aux[c2, G[T], G[T]] = ???
+    def apply[G[_]](func: Functor.Aux[G,c1.A,c2.A]) =
+      new IsObj[Functor.Aux[G,c1.A,c2.A]]:
+        type F[t] = G[t]
+        def idNat: Arrow.Id[Nat,Functor.Aux[F,c1.A, c2.A]] =
+          new Nat:
+            type F1[t] = F[t]
+            type F2[t] = F[t]
+            def apply[T](using obj: c1.IsObj[T]): Arrow.Aux[c2.A, F1[T], F2[T]] =
+              func(c1.id[T])
 
+  def id[T](using obj: IsObj[T]): Arrow.Id[Nat,T] = obj.idNat
+  def id(func: Arrow.Aux[Functor,c1.A,c2.A]): Arrow.Id[Nat, Functor.Aux[func.F,c1.A, c2.A] ] =
+    id(using IsObj(func))
 
-  def id[t](using o: IsObj[t]): Arrow.Id[A,t] = o.idNat
-
-  def compose(a1: A , a2: A ) (using proof: a2.Codom =:= a1.Dom):
-  A {type Dom = a2.Dom; type Codom = a1.Codom} =
-    new Nat[c1,c2]:
+  def compose(a1: A, a2: A)(using proof: a2.Codom =:= a1.Dom): Arrow.Aux[A, a2.Dom, a1.Codom] =
+    new Nat:
       self=>
       type F1[t] = a2.F1[t]
       type F2[t] = a1.F2[t]
-      def apply[T]: c2{type Dom = self.Dom#F[T]; type Codom = self.Codom#F[T] } =
-        val t2: Arrow.Aux[c2,a2.Dom#F[T], a2.Codom#F[T]] = a2[T]
-        val t1: Arrow.Aux[c2,a1.Dom#F[T], a1.Codom#F[T]] = a1[T]
+      def apply[T](using obj: c1.IsObj[T]): Arrow.Aux[c2.A, self.Dom#F[T], self.Codom#F[T]] =
+        val t2: Arrow.Aux[c2.A,a2.Dom#F[T], a2.Codom#F[T]] = a2[T]
+        val t1: Arrow.Aux[c2.A,a1.Dom#F[T], a1.Codom#F[T]] = a1[T]
         val postulateToWorkAroundTypeSystemLimitation: a2.Codom#F[T] =:= a1.Dom#F[T] = proof.asInstanceOf
-        val tst = postulateToWorkAroundTypeSystemLimitation.liftCo[[q]=>>Arrow.Aux[c2,a2.Dom#F[T], q]]
-        val t3: Arrow.Aux[c2, a2.F1[T], a1.F1[T]] = tst(t2)
-        val t4  = c2Cat.compose(t1,t3)
+        val tst = postulateToWorkAroundTypeSystemLimitation.liftCo[[q]=>>Arrow.Aux[c2.A,a2.Dom#F[T], q]]
+        val t3: Arrow.Aux[c2.A, a2.Dom#F[T], a1.Dom#F[T]] = tst(t2)
+        val t4: Arrow.Aux[c2.A, a2.Dom#F[T], a1.Codom#F[T]] = c2.compose(t1,t3)
         t4
