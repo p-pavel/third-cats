a987a73f9d2959da5004e49b6292c1ae1fab77fb
Pavel Perikov <perikov@gmail.com>
2022-06-06 14:50:58 +0400

    EmptyCategory

---
+++ src/main/scala/com/perikov/thirdcats/examples/EmptyCategory.scala
@@ -0,0 +1,17 @@
+package com.perikov.thirdcats.examples
+
+import com.perikov.thirdcats.{Arrow, Category}
+
+trait EmptyCategory extends Category :
+  opaque type IsObj[T] = Nothing
+
+  trait A extends Arrow :
+    val forbidden: Nothing //Can't make type A = Nothing https://github.com/lampepfl/dotty/issues/15377
+
+  override def dom(a: A): IsObj[A] = a.forbidden
+
+  override def codom(a: A): IsObj[A] = a.forbidden
+
+  override def compose(a1: A, a2: A)(using a2.Codom =:= a1.Dom): Arrow.Aux[A, a2.Dom, a1.Codom] = a1.forbidden
+
+  override def id[T](using o: IsObj[T]): Nothing = o
